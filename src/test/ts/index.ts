ebac9770f7e82cf2758b96d418eeb3c84c8f53fe
Anton Golub <mailbox@antongolub.ru>
2019-03-25 13:44:19 +0300

    style: self-linting

--- src/test/ts/index.ts
+++ src/test/ts/index.ts
@@ -1,6 +1,6 @@
-import { writeFile, readFile } from 'fs'
-import { exec } from 'child_process'
-import { join } from 'path'
+import {writeFile, readFile} from 'fs'
+import {exec} from 'child_process'
+import {join} from 'path'
 import * as config from '../../main/js/tslint-config-qiwi.js'
 
 const OUT_FILENAME = join(__dirname, 'rules.out')
@@ -10,15 +10,21 @@ const TSLINT_CMD = `node ${TSLINT_BIN} --config ../../main/js/tslint-config-qiwi
 /**
  * Remove all absolute paths when persisting.
  */
-const relatify =  (stdout, dirname) => {
+const relatify = (stdout, dirname) => {
   let index
+  const getIndex = () => {
+    index = stdout.indexOf(dirname)
+    return index
+  }
 
-  while ((index = stdout.indexOf(dirname)) > -1) {
+  while (getIndex() > -1) {
     stdout = stdout.substr(0, index) + stdout.substr(index + dirname.length)
   }
 
   return stdout
 }
+
+const handleError = (err) => !!err
 
 describe('tslint-config-qiwi', () => {
   it('config is exposed as js', () => {
@@ -26,23 +32,26 @@ describe('tslint-config-qiwi', () => {
   })
 
   it('config works as expected', (done) => {
-    exec(TSLINT_CMD, { cwd: __dirname }, (err, stdout, stderr) => {
+    exec(TSLINT_CMD, {cwd: __dirname}, (err, stdout, stderr) => {
+      handleError(err)
+
       const out = relatify(stdout, __dirname).trim()
 
       if (process.env.GENERATE_ASSETS) {
         return writeFile(OUT_FILENAME, out, (err) => {
-          done()
-        })
-
-      } else {
-        readFile(OUT_FILENAME, 'utf8', (err, result) => {
-          expect(out.trim()).toEqual(result.trim())
-          expect(stderr).toEqual('')
-
+          handleError(err)
           done()
         })
       }
+
+      readFile(OUT_FILENAME, 'utf8', (err, result) => {
+        handleError(err)
+
+        expect(out.trim()).toEqual(result.trim())
+        expect(stderr).toEqual('')
+
+        done()
+      })
     })
   })
 })
-
