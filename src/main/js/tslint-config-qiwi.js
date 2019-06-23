71a886353f9061031bca99af8cfdb7796180e909
Anton Golub <mailbox@antongolub.ru>
2019-06-23 19:35:10 +0300

    feat: apply rules to JS files

--- src/main/js/tslint-config-qiwi.js
+++ src/main/js/tslint-config-qiwi.js
@@ -1,63 +1,66 @@
-module.exports = {
-  "extends": [
-    "tslint-config-standard",
-    "tslint-react"
+const rules = {
+  'brace-style': [
+    true,
+    'stroustrup',
   ],
-  "rules": {
-    "brace-style": [
-      true,
-      "stroustrup"
-    ],
-    "jsx-boolean-value": false,
-    "jsx-no-lambda": false,
-    "jsx-no-multiline-js": false,
-    "no-consecutive-blank-lines": [
-      true,
-      1
-    ],
-    "object-curly-spacing": [
-      true,
-      "never"
-    ],
-    "object-literal-shorthand": true,
-    "one-line": [
-      false
-    ],
-    "ter-padded-blocks": [
-      true,
-      {
-        "classes": "always"
-      }
-    ],
-    "prefer-object-spread": true,
-    "quotemark": [
-      true,
-      "single",
-      "jsx-double"
-    ],
-    "space-before-function-paren": [
-      true,
-      {
-        "anonymous": "never",
-        "asyncArrow": "never",
-        "constructor": "never",
-        "method": "never",
-        "named": "never"
-      }
-    ],
-    "trailing-comma": [
-      true,
-      {
-        "multiline": {
-          "arrays": "always",
-          "objects": "always",
-          "functions": "always",
-          "imports": "always",
-          "exports": "always",
-          "typeLiterals": "never"
-        },
-        "singleline": "never"
-      }
-    ]
-  }
+  'jsx-boolean-value': false,
+  'jsx-no-lambda': false,
+  'jsx-no-multiline-js': false,
+  'no-consecutive-blank-lines': [
+    true,
+    1,
+  ],
+  'object-curly-spacing': [
+    true,
+    'never',
+  ],
+  'object-literal-shorthand': true,
+  'one-line': [
+    false,
+  ],
+  'ter-padded-blocks': [
+    true,
+    {
+      'classes': 'always',
+    },
+  ],
+  'prefer-object-spread': true,
+  'quotemark': [
+    true,
+    'single',
+    'jsx-double',
+  ],
+  'space-before-function-paren': [
+    true,
+    {
+      'anonymous': 'never',
+      'asyncArrow': 'never',
+      'constructor': 'never',
+      'method': 'never',
+      'named': 'never',
+    },
+  ],
+  'trailing-comma': [
+    true,
+    {
+      'multiline': {
+        'arrays': 'always',
+        'objects': 'always',
+        'functions': 'always',
+        'imports': 'always',
+        'exports': 'always',
+        'typeLiterals': 'never',
+      },
+      'singleline': 'never',
+    },
+  ],
+}
+
+module.exports = {
+  extends: [
+    'tslint-config-standard',
+    'tslint-react',
+  ],
+  jsRules: rules,
+  rules,
 }
