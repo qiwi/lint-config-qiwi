bf66c996ab86c1af1dc2bac800aa9c45eaa1bcbe
Anton Golub <mailbox@antongolub.ru>
2019-09-27 16:07:37 +0300

    test: migrate to stdstream-snapshot

--- packages/tslint-config-qiwi/src/test/ts/index.ts
+++ packages/tslint-config-qiwi/src/test/ts/index.ts
@@ -1,57 +1,23 @@
-import {writeFile, readFile} from 'fs'
-import {exec} from 'child_process'
-import {join} from 'path'
+import {generateSnapshot} from 'stdstream-snapshot'
+import {resolve} from 'path'
 import * as config from '../../main/js/tslint-config-qiwi.js'
-
-const OUT_FILENAME = join(__dirname, 'rules.out')
-const TSLINT_BIN = require.resolve('tslint/bin/tslint')
-const TSLINT_CMD = `node ${TSLINT_BIN} --config ../../main/js/tslint-config-qiwi.js --project tsconfig.json "rules/**/*.ts"`
-
-/**
- * Remove all absolute paths when persisting.
- */
-const relatify = (stdout, dirname) => {
-  let index
-  const getIndex = () => {
-    index = stdout.indexOf(dirname)
-    return index
-  }
-
-  while (getIndex() > -1) {
-    stdout = stdout.substr(0, index) + stdout.substr(index + dirname.length)
-  }
-
-  return stdout
-}
-
-const handleError = (err) => !!err
 
 describe('tslint-config-qiwi', () => {
   it('config is exposed as js', () => {
     expect(config).toEqual(expect.any(Object))
   })
 
-  it('config works as expected', (done) => {
-    exec(TSLINT_CMD, {cwd: __dirname}, (err, stdout, stderr) => {
-      handleError(err)
+  it('config works as expected', async() => {
+    const configPath = resolve(__dirname, '../../main/js/tslint-config-qiwi.js')
+    const rulesPath = resolve(__dirname, 'rules/*.ts')
+    const tsconfigPath = resolve(__dirname, 'tsconfig.json')
+    const tslint = require.resolve('tslint/bin/tslint')
+    const cmd = `node ${tslint} --config ${configPath} --project ${tsconfigPath} ${rulesPath}`
 
-      const out = relatify(stdout, __dirname).trim()
-
-      if (process.env.GENERATE_ASSETS) {
-        return writeFile(OUT_FILENAME, out, (err) => {
-          handleError(err)
-          done()
-        })
-      }
-
-      readFile(OUT_FILENAME, 'utf8', (err, result) => {
-        handleError(err)
-
-        expect(out.trim()).toEqual(result.trim())
-        expect(stderr).toEqual('')
-
-        done()
-      })
+    const result = await generateSnapshot({
+      cmd,
     })
+
+    expect(result).toMatchSnapshot()
   })
 })
