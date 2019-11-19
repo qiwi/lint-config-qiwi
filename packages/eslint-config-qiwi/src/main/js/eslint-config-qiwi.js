ecfe9a3809e68c3fbcfd2d45fc256aa97d6d24e8
Maxim Pismenskiy <m.pismenskiy@qiwi.com>
2019-11-19 11:24:25 +0300

    fix(eslint): let imported classes be used as type refs
    closes #15

--- packages/eslint-config-qiwi/src/main/js/eslint-config-qiwi.js
+++ packages/eslint-config-qiwi/src/main/js/eslint-config-qiwi.js
@@ -4,25 +4,95 @@ module.exports = {
     browser: true,
     es6: true
   },
+  parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
-    'standard'
+    'standard',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:array-func/recommended'
   ],
-  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-hooks',
+    'optimize-regex',
+    'sonarjs',
+    'unicorn'
+  ],
+  rules: {
+    'eslint/no-unused-vars': 0,
+    'optimize-regex/optimize-regex': 'warn',
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    'unicorn/consistent-function-scoping': 'error',
+    'unicorn/custom-error-definition': 'off',
+    'unicorn/error-message': 'error',
+    'unicorn/escape-case': 'error',
+    'unicorn/explicit-length-check': 'error',
+    'unicorn/import-index': 'error',
+    'unicorn/new-for-builtins': 'error',
+    'unicorn/no-array-instanceof': 'error',
+    'unicorn/no-console-spaces': 'error',
+    'unicorn/no-fn-reference-in-iterator': 'off',
+    'unicorn/no-for-loop': 'error',
+    'unicorn/no-hex-escape': 'error',
+    'unicorn/no-keyword-prefix': 'off',
+    'no-nested-ternary': 'off',
+    'unicorn/no-new-buffer': 'error',
+    'unicorn/no-process-exit': 'error',
+    'unicorn/no-unreadable-array-destructuring': 'error',
+    'unicorn/no-unsafe-regex': 'off',
+    'unicorn/no-unused-properties': 'off',
+    'unicorn/no-zero-fractions': 'error',
+    'unicorn/number-literal-case': 'error',
+    'unicorn/prefer-add-event-listener': 'error',
+    'unicorn/prefer-dataset': 'error',
+    'unicorn/prefer-event-key': 'error',
+    'unicorn/prefer-exponentiation-operator': 'error',
+    'unicorn/prefer-flat-map': 'error',
+    'unicorn/prefer-includes': 'error',
+    'unicorn/prefer-node-append': 'error',
+    'unicorn/prefer-node-remove': 'error',
+    'unicorn/prefer-query-selector': 'error',
+    'unicorn/prefer-reflect-apply': 'error',
+    'unicorn/prefer-spread': 'error',
+    'unicorn/prefer-starts-ends-with': 'error',
+    'unicorn/prefer-text-content': 'error',
+    'unicorn/prefer-type-error': 'error',
+    'unicorn/regex-shorthand': 'error',
+    'unicorn/throw-new-error': 'error',
+    'array-func/prefer-array-from': 0,
+    'sonarjs/cognitive-complexity': 0,
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off'
+  },
+  globals: {
+    Cypress: 'readonly',
+    cy: 'readonly',
+    it: 'readonly',
+    expect: 'readonly',
+    describe: 'readonly',
+    fetch: 'readonly'
+  },
+  settings: {
+    react: {
+      version: 'latest'
+    }
+  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 9,
     sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'react-hooks'
-  ],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
   }
 }
