82fe590a7d95e89db6b3710fca2f78483889b616
Anton Golub <antongolub@antongolub.com>
2023-03-14 15:12:06 +0300

    feat: refactor ts and js rules (#183)

--- packages/eslint-config-qiwi/src/test/js/index.js
+++ packages/eslint-config-qiwi/src/test/js/index.js
@@ -4,17 +4,17 @@ import { generateSnapshot } from '@qiwi/stdstream-snapshot'
 
 import * as config from '../../main/js/index.js'
 
-describe('eslint-config-qiwi (TypeScript)', () => {
+describe('eslint-config-qiwi (JavaScript)', () => {
   it('config is exposed as js', () => {
     expect(config).toEqual(expect.any(Object))
   })
 
   it('config works as expected', async () => {
     const configPath = resolve(__dirname, '../../main/js/index.js')
-    const rulesPath = resolve(__dirname, 'rules/*.ts')
+    const rulesPath = resolve(__dirname, 'rules/*.js')
     const eslint = resolve(__dirname, '../../../../../node_modules/.bin/eslint')
     const result = await generateSnapshot({
-      cmd: `${eslint} --config ${configPath} ${rulesPath} --no-ignore`,
+      cmd: `${eslint} -c ${configPath} ${rulesPath} --no-ignore`,
     })
 
     expect(result).toMatchSnapshot()
