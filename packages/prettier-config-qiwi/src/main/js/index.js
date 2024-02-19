9e08b3c554b134c092567bee7a0e2fe8f8537cdd
Dima Livshits <dimonka83@gmail.com>
2024-02-19 17:11:07 +0300

    feat: up deps

--- packages/prettier-config-qiwi/src/main/js/index.js
+++ packages/prettier-config-qiwi/src/main/js/index.js
@@ -2,10 +2,16 @@ module.exports = {
   trailingComma: 'all',
   semi: false,
   singleQuote: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
   importOrder: ['^node:', '^react', '^@qiwi/', '^@pijma/', '^[^./]', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
   importOrderCaseInsensitive: true,
   importOrderGroupNamespaceSpecifiers: true,
-  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy', 'importAssertions'],
+  importOrderParserPlugins: [
+    'typescript',
+    'jsx',
+    'decorators-legacy',
+    'importAssertions',
+  ],
 }
