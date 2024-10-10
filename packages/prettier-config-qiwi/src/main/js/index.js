8b65dec59a43f44a6bc42717eb968a04cab8bd88
Anton Golub <antongolub@antongolub.com>
2024-10-10 19:33:49 +0300

    fix: enable @trivago/prettier-plugin-sort-imports

--- packages/prettier-config-qiwi/src/main/js/index.js
+++ packages/prettier-config-qiwi/src/main/js/index.js
@@ -2,6 +2,7 @@ module.exports = {
   trailingComma: 'all',
   semi: false,
   singleQuote: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
   importOrder: ['^node:', '^react', '^@qiwi/', '^@pijma/', '^[^./]', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
